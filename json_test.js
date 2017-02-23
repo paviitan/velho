cad3e4af2ba51e199daacb3b8a4838170485f8a0
Pasi Viitanen <viitanen.pasi@outlook.com>
2017-02-23 14:12:42 +0200

    web interface

--- json_test.js
+++ json_test.js
@@ -1,38 +1,55 @@
 // secrets for API keys and such
 var secrets = require('./.gitignore/secrets.json')
-var fs = require('fs')
 var FeatureCollection = {"type":"FeatureCollection","features":[]}
-//prototype for feature
-var feature = {
+var fs = require('fs')
+var geofilelocation = './maps/geo.json'
+
+
+//puts together feature information
+function weaver (id, latitude, longitude, temperature) {
+	id = parseInt(id,16)
+	return feature = {
 	"type":"Feature",
-	"geometry":{"type":"Point", "coordinates":[0,0]},
-	"properties":{"temperature": 0.0},
-	"id": 0
-	}
+	"geometry":{"type":"Point", "coordinates":[longitude, latitude]},
+	"properties":{"temperature": temperature},
+	"id": id}
+}
 	
 var mqtt = require('mqtt')
 var client  = mqtt.connect('mqtt://169.50.27.27')
 var topics = ['devices/1','devices/20','devices/100']
 
-//simulated JSON parsed message	
-var my_message = {"id":2 , "longitude":36.24225, "latitude":42.75908, "temperature":25.1}
-//id to base10
-my_message.id = parseInt(my_message.id,16)
-console.log("Datapoint: ", my_message.id, "Temperature: ", my_message.temperature, " ºC")
-// build feature and push to collection
-feature.id = my_message.id
-feature.properties.temperature = my_message.temperature
-feature.geometry.coordinates[0] = my_message.latitude
-feature.geometry.coordinates[1] = my_message.longitude
-FeatureCollection.features.push(feature)
-// test prints
-console.log(feature.geometry)
-console.log(feature.properties)
+//populate list of topics
+/*
+var topics = []
+for(var i = 0; i < 10; i++){topics[i] = "devices/".concat(i)}
+console.log(topics.length)
+*/
+client.on('connect', function () {
+  client.subscribe(topics)
+  //client.publish('presence', 'Hello mqtt')
+})
+console.log("Subattu topikkeihin: ", topics.toString())
+console.log("Haetaan viestejä, odota pari minuuttia...")
 
-//print FeatureCollection
-console.log(FeatureCollection)
-var json = JSON.stringify(FeatureCollection)
-fs.writeFile('geo.json', json, 'utf8')
+client.on('message', function (topic, message) {
+	/*  id, latitude, longitude, temperature  */
+	/* JSON format */
+
+	var my_message = JSON.parse(message)
+	//id to base10
+	console.log("Datapoint: ", my_message.id, "Temperature: ", my_message.temperature, " ºC")
+	// build feature and push to collection
+	var feature = weaver(my_message.id, my_message.latitude, my_message.longitude, my_message.temperature)
+	FeatureCollection.features.push(feature)
+	client.end()
+  if (topics.length == FeatureCollection.features.length){
+	//export to json file if all topics are gathered
+	console.log(FeatureCollection)
+	var jsonfile = JSON.stringify(FeatureCollection)
+	fs.writeFile(geofilelocation, jsonfile, 'utf8')
+}
+})
 
 
 
